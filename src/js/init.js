81fe95b8790adbc17087aa5b53830f8ae9fd1cee
hunter-s70 <serg.buhtoyarov@gmail.com>
2016-05-03 01:42:54 +0400

    change init files

--- src/js/init.js
+++ src/js/init.js
@@ -1,3 +1,9 @@
-$("document").ready(function() {
-
+$(document).ready(function() {
+    var elem = document.getElementById('canvas');
+    var canvas = elem.getContext("2d");
+    canvas.fillStyle="#000";
+    canvas.beginPath();
+    canvas.arc(100,100,20,0,Math.PI*2,false);
+    canvas.fill();
+    canvas.closePath();
 });
