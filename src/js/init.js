65f2ee594fbda49f97b2b52131230cb8ddb38dc1
hunter-s70 <serg.buhtoyarov@gmail.com>
2017-08-26 22:56:05 +0400

    clear repo

--- src/js/init.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    var elem = document.getElementById('canvas');
-    var canvas = elem.getContext("2d");
-    canvas.fillStyle="#000";
-    canvas.beginPath();
-    canvas.arc(100,100,20,0,Math.PI*2,false);
-    canvas.fill();
-    canvas.closePath();
-});
