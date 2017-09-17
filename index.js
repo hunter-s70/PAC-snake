7698e17e4113d0f24549ac7633411deb03b4c543
hunter-s70 <serg.buhtoyarov@gmail.com>
2017-09-17 18:43:23 +0300

    change structure for heroku

---
+++ index.js
@@ -0,0 +1,18 @@
+/**
+ * Created by hunter_s70 on 10.12.2016.
+ */
+var express = require("express");
+var app = express();
+app.use(express.logger());
+
+app.use(express.static('web'));
+app.use(express.static('node_modules'));
+
+app.get('/', function(request, response) {
+    response.send('Pac_snake game');
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+    console.log("Listening on " + port);
+});
